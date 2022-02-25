7541d6cf54df06f119c741060964e228af4bc04d
BrandonSev <seveste.brandon@gmail.com>
2022-02-25 17:36:52 +0100

    fix history local

--- src/middleware/Images/index.js
+++ src/middleware/Images/index.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 const { Images } = require("../../models");
+const { breakpointList } = require("../../controllers/images.controllers");
 
 const validatePutImages = (req, res, next) => {
   const { project_id } = req.body;
@@ -24,6 +25,16 @@ const deletePrevImage = async (req, res, next) => {
     if (!image) return res.status(404).send();
     return fs.unlink(path.join(__dirname, `../../../public/images/${image.src}`), (err) => {
       if (err) return res.status(500).send();
+      breakpointList.forEach((breakpoint) => {
+        // eslint-disable-next-line consistent-return
+        fs.unlink(
+          path.join(__dirname, `../../../public/images/${image.src.split(".")[0]}-${breakpoint.break}.${image.src.split(".")[1]}`),
+          // eslint-disable-next-line consistent-return
+          (err2) => {
+            if (err2) return res.status(500).send();
+          },
+        );
+      });
       return next();
     });
   } catch (e) {
